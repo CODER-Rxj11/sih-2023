24ebd4d94607520dc39333872acb8dd37400ef24
CODER-Rxj11 <rajkm9111@gmail.com>
2023-09-19 18:32:39 +0530

    page commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,26 +3,19 @@
 import Navbar from "../../components/Navbar"
 import Slide from "../../components/Slide"
 import Welcome from "../../components/Welcome"
-<<<<<<< HEAD
 import Footer from "../../components/Footer";
 import MapComponent from "../../components/MapComponent";
 import CarouselComponent from "../../components/CarouselComponent";
-=======
 import Auth from "../../components/Auth"
-import Footer from "../../components/Footer"
->>>>>>> bb05925dac71911c522dccdd45999f3d02a2029e
 
 export default function Home() {
-  return <>
+  return <div className="flex flex-col overflow-x-hidden">
     <Navbar />
     <Slide />
     <Welcome />
-<<<<<<< HEAD
     <CarouselComponent />
     <MapComponent />
-=======
     <Auth />
->>>>>>> bb05925dac71911c522dccdd45999f3d02a2029e
     <Footer />
-  </>
+  </div>
 }
